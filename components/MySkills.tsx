313d213781c05139ca7d5fe293ea5a63e6d562b3
Samuel Preston <samp.reston@outlook.com>
2022-08-24 21:42:15 +0100

    Fix lopsided

--- components/MySkills.tsx
+++ components/MySkills.tsx
@@ -6,7 +6,7 @@ import SimpleIconsTrpc from './SimpleIconsTrpc'
 const MySkills: FunctionComponent = () => {
 
   return (
-    <div className='bg-white 2xl:px-64 xl:px-64 lg:px-32 md:px-16 px-8 pb-16 min-w-full transition-all ease-linear duration-300 py-32 gap-24 items-center flex flex-col'>
+    <div className='bg-white 2xl:px-64 xl:px-64 lg:px-32 md:px-16 px-8 min-w-full transition-all ease-linear duration-300 py-32 gap-24 items-center flex flex-col'>
       <section className='flex flex-col items-center gap-6'>
         <h4 className='text-black-90 md:text-h4 text-h4-mobile'>The Tools I Use</h4>
         <ul className='flex flex-wrap gap-6 justify-center md:text-p text-p-mobile text-black-90'>
