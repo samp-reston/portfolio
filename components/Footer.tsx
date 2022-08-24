e38ec35041df92afbb23b6835768e2bc8fe0b471
Samuel Preston <samp.reston@outlook.com>
2022-08-24 14:13:22 +0100

    Finalise Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { SiGithub, SiLinkedin, SiTwitter } from 'react-icons/Si'
 const Footer: FunctionComponent = () => {
   const email: string = 'samp.reston@outlook.com'
   return (
-    <footer className='flex flex-col gap-8 pt-16 sm:pl-16'>
+    <footer className='flex flex-col gap-8 pt-16 sm:pt-0 sm:pl-16'>
       <p className='text-black-90 text-p'>Designed and Built by Samuel Preston</p>
       <p className='text-black-90 text-p'><a href={`emailto:${email}`}>{email}</a></p>
       <ul className='flex flex-row gap-6 text-socials text-black-90'>
