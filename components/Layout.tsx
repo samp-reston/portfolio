02ffe944dd97425a30b3936f31ebad121517589e
Samuel Preston <samp.reston@outlook.com>
2022-08-24 01:16:20 +0100

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ type PropTypes = {
 
 const Layout: FunctionComponent<PropTypes> = ({children}) => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-primary to-base flex flex-col items-start 2xl:px-64 xl:px-64 lg:px-32 md:px-16 px-8 gap-4 pb-16 min-w-full transition-all ease-linear duration-500">
+    <div className="min-h-screen bg-gradient-to-b from-primary to-base flex flex-col items-start 2xl:px-64 xl:px-64 lg:px-32 md:px-16 px-8 gap-4 pb-16 min-w-full transition-all ease-linear duration-300">
       <Header />
       <main>{children}</main>
       <Footer />
