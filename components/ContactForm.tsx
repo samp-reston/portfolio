31a9f7e433bd7d7a0598539a00cb0cba575df8ca
Samuel Preston <samp.reston@outlook.com>
2022-08-26 14:30:04 +0100

    fix form validation

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -4,8 +4,9 @@ import React, { useState, FunctionComponent } from 'react'
 import { useFormspark } from '@formspark/use-formspark'
 import { useForm } from 'react-hook-form'
 import z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export type SubmitContactForm = z.TypeOf<typeof submitContactForm>
+export type SubmitContactFormType = z.TypeOf<typeof submitContactForm>
 
 export const submitContactForm = z.object({
   name: z.string().min(1),
@@ -14,14 +15,14 @@ export const submitContactForm = z.object({
 })
 
 const ContactForm: FunctionComponent = () => {
-  const { handleSubmit, register, formState: { errors, isSubmitted }, resetField } = useForm<SubmitContactForm>()
+  const { handleSubmit, register, formState: { errors, isSubmitSuccessful, isSubmitting }, resetField } = useForm<SubmitContactFormType>({ resolver: zodResolver(submitContactForm) })
   const [submit, submitting] = useFormspark({
     formId: 'SdhXPyCc'
   })
 
-  const onSubmit = (values: SubmitContactForm): void => {
+  const onSubmit = (values: SubmitContactFormType): void => {
     submit(values)
-      .catch((err) => console.log(err))
+      .catch((err) => { throw new Error(err) })
     resetField('name')
     resetField('email')
     resetField('message')
@@ -32,17 +33,17 @@ const ContactForm: FunctionComponent = () => {
     <form onSubmit={handleSubmit(onSubmit)} className='flex grow flex-col gap-4 text-p-mobile md:text-p'>
       <div className='flex flex-col'>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" {...register('name', { required: true })} className={`h-12 rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.name != null) ? 'border-error' : ''}`}/>
+        <input id="name" type="text" {...register('name', { required: true })} disabled={isSubmitting} className={`h-12 rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.name != null) ? 'border-error' : ''}`}/>
       </div>
       <div className='flex flex-col'>
         <label htmlFor="email">Email</label>
-        <input id="email" type="text" {...register('email', { required: true })} className={`h-12 rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.email != null) ? 'border-error' : ''}`}/>
+        <input id="email" type="text" {...register('email', { required: true })} disabled={isSubmitting} className={`h-12 rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.email != null) ? 'border-error' : ''}`}/>
       </div>
       <div className='flex flex-col'>
         <label htmlFor="message">Message</label>
-        <textarea id="message" cols={30} rows={7} {...register('message', { required: true })} className={`rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.message != null) ? 'border-error' : ''}`}></textarea>
+        <textarea id="message" cols={30} rows={7} {...register('message', { required: true })} disabled={isSubmitting} className={`rounded-lg border-4 border-secondary bg-black-75 p-1 text-white focus:outline-none ${(errors.message != null) ? 'border-error' : ''}`}></textarea>
       </div>
-      <input type="submit" className={`h-12 w-1/2 self-end rounded-lg border-4 border-secondary bg-black-75 p-1 text-white transition-all duration-200 hover:cursor-pointer hover:text-secondary active:bg-black-5 active:text-black-100 active:outline-none ${isSubmitted ? 'border-green-500' : ''}`} value='Send'/>
+      <input type="submit" className={`h-12 w-1/2 self-end rounded-lg border-4 border-secondary bg-black-75 p-1 text-white transition-all duration-200 hover:cursor-pointer hover:text-secondary active:bg-black-5 active:text-black-100 active:outline-none ${isSubmitSuccessful ? 'border-green-500' : ''}`} value='Send'/>
     </form>
   )
 }
